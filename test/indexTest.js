2ba2778755a0ac4c3abca273614a40e9ae948628
Audreymary <audreymoraa006@gmail.com>
2024-09-16 11:45:37 +0300

    add indexTest.js

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,25 +1,25 @@
-require ( './root.js' );
+require('./root.js');
 
 const { name, height, message } = require("../index.js");
 
 describe("what-is-a-test", () => {
   describe("Name", () => {
-    it('returns "Susan"', () => {
-      expect(name).toEqual("Susan");
+    it('returns "Joe"', () => {
+      expect(name).toEqual("Joe");
     });
   });
 
   describe("Height", () => {
     it("is less than 40 and greater than 0", () => {
-      expect(height).toBeMoreThan(0)
+      expect(height).toBeGreaterThan(0);
       expect(height).toBeLessThan(40);
     });
   });
 
   describe("Message", () => {
     it("gives the name and height", () => {
-      expect(message).toInclude(name);
-      expect(message).toInclude(height);
+      expect(message).toInclude("Joe");
+      expect(message).toInclude(height.toString()); // Convert height to string for comparison
     });
   });
 });
